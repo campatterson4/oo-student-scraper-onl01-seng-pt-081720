1981d8a51f2d81ce844c461d20d30ec554096807
Camryn Patterson <camrynpatterson@yahoo.com>
2020-10-09 19:36:09 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,25 +3,25 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    html = open(index_url)
-    doc = Nokogiri::HTML(html)
-    student_cards = doc.css(".student-card a")
+def self.scrape_index_page(index_url)
+  html = open(index_url)
+doc = Nokogiri::HTML(html)
+  student_cards = doc.css(".student-card a")
     student_cards.collect do |element|
       {:name => element.css(".student-name").text ,
         :location => element.css(".student-location").text,
         :profile_url => element.attr('href')
       }
-    end
-  end
+end
+end
 
-  def self.scrape_profile_page(profile_url)
+def self.scrape_profile_page(profile_url)
     html = open(profile_url)
     doc = Nokogiri::HTML(html)
     return_hash = {}
 
-      social = doc.css(".vitals-container .social-icon-container a")
-      social.each do |element|
+  social = doc.css(".vitals-container .social-icon-container a")
+  social.each do |element|
         if element.attr('href').include?("twitter")
           return_hash[:twitter] = element.attr('href')
         elsif element.attr('href').include?("linkedin")
